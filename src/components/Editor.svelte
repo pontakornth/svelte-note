766a2c5eaeae30c016c22fa328bbe0b9adbc1059
pontakornth <pontakorn61@gmail.com>
2020-04-28 13:42:00 +0700

    feat(editor):  Add save (unimplemented) and reset button

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,14 +1,21 @@
 <script>
 import Preview from './Preview.svelte';
 let content = '';
+$: isContentEmpty = content.length  === 0;
+const resetContent = () => {
+  content = ''
+}
 </script>
 
 <div class="editor">
   <h1 class="title is-1 has-text-centered">ตัวแก้ไข Markdown</h1>
   <textarea class="textarea" placeholder="Your note here" bind:value={content} />
 </div>
-
 <Preview {content} />
+<div class="buttons">
+<button class="button is-success">Save</button>
+<button class="button is-danger" disabled={isContentEmpty} on:click={resetContent}>Reset</button>
+</div>
 
 <style lang="scss">
 .editor {
