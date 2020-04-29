d1f0aa42d0bc4fde95eb13a363e878134df24bd2
pontakornth <pontakorn61@gmail.com>
2020-04-29 16:05:48 +0700

    feat(menu): Add delete button

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -11,9 +11,14 @@ const changePage = (index) => {
 </div>
 {#if $notes.pages.length > 0}
   {#each $notes.pages as page, index (page.uuid)}
-  <a href="javascript:void(0)" class="item" on:click={changePage(index)} class:is-active={$notes.currentIndex === index }>
-    {page.title}
-  </a>
+  <div class="item">
+    <a href="javascript:void(0)" class:is-active={$notes.currentIndex === index } on:click={changePage(index)}>{page.title}</a>
+    <a href="javascript:void(0)" class:is-visible={$notes.currentIndex === index } class="delete-button has-text-danger">
+      <span class="icon">
+        <span class="material-icons">clear</span>
+      </span>
+    </a>
+  </div>
   {/each}
 {:else}
   <div class="item placeholder">No notes here</div>
@@ -21,20 +26,33 @@ const changePage = (index) => {
 
 <style lang="scss">
 .item {
-  display: block;
+  display: flex;
+  justify-content: space-between;
   width: 100%;
   padding: 0.5rem;
   background: #eee;
   color: #111;
-  &.is-active {
+  & a {
+    color: #111;
+    
+  }
+  & a.is-active {
   color: hsl(217, 71%, 53%);
   }
 
   &.placeholder {
     color: rgb(44,44,44);
   }
+  &:hover {
+    .delete-button {
+      display: inline-flex;
+    }
+  }
 }
 
+.delete-button {
+  display: none;
+}
 .padded {
   padding: 0.5rem;
 }
